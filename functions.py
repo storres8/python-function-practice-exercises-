edaa4fc240b4e591a6d4f4943b92f24153b705c2
storres8 <stevetorres7562@gmail.com>
2018-10-16 10:45:35 -0400

    built almost there function

--- functions.py
+++ functions.py
@@ -30,3 +30,16 @@ def master_yoda(string):
   b = ' '.join(a)
 
   return b
+
+# ALMOST THERE: Given an integer n, return True if n is within 10 of either 100 or 200
+# almost_there(90) --> True
+# almost_there(104) --> True
+# almost_there(150) --> False
+# almost_there(209) --> True
+def almost_there(n):
+  if n >= 90 and n <= 110:
+    return True
+  elif n >= 190 and n <= 210:
+    return True
+  else:
+    return False
