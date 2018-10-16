a102969e7fef48039d0ce6b2cf702def1ce06c25
storres8 <stevetorres7562@gmail.com>
2018-10-16 11:38:16 -0400

    built spy_game function

--- functions.py
+++ functions.py
@@ -103,6 +103,20 @@ def summer_69(arr):
                 break
             else:
                 add = True
-                break
-    return total
-    
+                breakdef spy_game(nums):
+
+
+# SPY GAME: Write a function that takes in a list of integers and returns True if it contains 007 in order
+#  spy_game([1,2,4,0,0,7,5]) --> True
+#  spy_game([1,0,2,4,0,5,7]) --> True
+#  spy_game([1,7,2,0,4,5,0]) --> False
+
+    def spy_game(nums):
+
+    code = [0,0,7,'x']
+    # used a code list to compare numbers that were being taken in by the function 
+    for num in nums:
+        if num == code[0]:
+            code.pop(0)   # code.remove(num) also works
+
+    return len(code) == 1
